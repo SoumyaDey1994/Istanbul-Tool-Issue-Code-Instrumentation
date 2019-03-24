dc49ee53ccfc50c47cfd1e5f2a1695225ff51588
Soumya Dey <deysoumya31@gmail.com>
2019-03-24 23:04:48 +0530

    Istanbul tool issue

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ class Child extends Parent{
 
     getSum(arg1, arg2){
         /**
-         * Will cause an error
+         * Will cause an error by istanbul
          * as at runtime new (super.service)(arg1, arg2) gets converted to => new super.service(arg1, arg2)
          * Which causes error " Unsupported reference to 'super' "
          */
-        // const service = new (super.service)(arg1,arg2);
+         //const service = new super.service(arg1,arg2);  // Will cause error by node
+         // const service = new (super.service)(arg1,arg2);
         const service = new (super.service.bind(this))(arg1, arg2);    //Will run sucessfully 
         return service.addService();
     }
